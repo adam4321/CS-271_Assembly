219469bef7609ffff1675f3332cb9e8f7db153e8
Adam Wright <adam@adamjwright.com>
2020-01-08 02:19:41 -0800

    Initial commit of skeleton file

---
+++ Program-1/Program-1.asm
@@ -0,0 +1,33 @@
+TITLE Program 1
+
+; Author:						Adam Wright
+; Last Modified:				1-8-2020
+; OSU email address:			wrighada@oregonstate.edu
+; Course number/section:		cs271--400
+; Project Number:               1  
+; Due Date:						1-19-2020
+; Description:					Assembly program which prompts the user for
+;								three integers in descending order and then
+;								calculates the sum and difference of the sets
+;								(A+B, A-B, A+C, A-C, B+C, B-C, A+B+C) and 
+;								displays it to the user
+
+INCLUDE Irvine32.inc
+
+; (insert constant definitions here)
+
+.data
+
+; (insert variable definitions here)
+
+.code
+main PROC
+
+; (insert executable instructions here)
+
+	exit	; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+
+END main
