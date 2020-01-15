d778ec883cfec6019f8f57a84c7218c494e0e083
Adam Wright <adam@adamjwright.com>
2020-01-15 00:22:09 -0800

    handle negative numbers

--- Program-1/Project-1.asm
+++ Program-1/Project-1.asm
@@ -14,11 +14,14 @@ TITLE Program 1  -- Sums and Differences
 
 INCLUDE Irvine32.inc
 
+
 ; Variable definitions
+
 .data
 intro		BYTE	"Adam Wright  --  Program-1 -- Sums and differences", 0
-extCred1	BYTE	"**EC: Program repeats until the user chooses to quit", 0
-extCred2	BYTE	"**EC: Program verifies the numbers are in descending order", 0
+extCred1	BYTE	"**EC-1: Program repeats until the user chooses to quit", 0
+extCred2	BYTE	"**EC-2: Program verifies the numbers are in descending order", 0
+extCred3	BYTE	"**EC-3: Handles negative results and computes B-A, C-A, C-B, C-B-A", 0
 instrctMsg	BYTE	"Enter 3 numbers A > B > C, and I'll show you the sums and differences!", 0
 firPrompt	BYTE	"First number: ", 0
 secPrompt	BYTE	"Second number: ", 0
@@ -29,15 +32,17 @@ addSym		BYTE	" + ", 0
 subSym		BYTE	" - ", 0
 eqlSym		BYTE	" = ", 0
 goodBye		BYTE	"Good-bye !!!", 0
-numA		DWORD	?								; Integer A to be entered by user
-numB		DWORD	?								; Integer B to be entered by user
-numC		DWORD	?								; Integer C to be entered by user
+numA		SDWORD	?								; Integer A to be entered by user
+numB		SDWORD	?								; Integer B to be entered by user
+numC		SDWORD	?								; Integer C to be entered by user
+
 
 ; Executable instructions
+
 .code  
 main PROC
 
-; Introduce programmer and title
+; Introduce programmer, title, and extra credit options
 	call	CrLf
 	mov		edx, OFFSET intro
 	call	WriteString
@@ -48,6 +53,9 @@ main PROC
 	mov		edx, OFFSET extCred2
 	call	WriteString
 	call	CrLf
+	mov		edx, OFFSET extCred3
+	call	WriteString
+	call	CrLf
 	call	CrLf
 
 ; Print the instructions
@@ -55,9 +63,10 @@ main PROC
 	call	WriteString
 	call	CrLf
 
-START_LOOP:											; Start of program loop
 
-; Prompt for first number							; Numbers prompts
+START_LOOP:											; User can press 2 and continue with JMP From: line-271
+
+; Prompt for first number							; Prompt for 3 numbers
 	call	CrLf
 	mov		edx, OFFSET firPrompt
 	call	WriteString
@@ -86,113 +95,176 @@ START_LOOP:											; Start of program loop
 	jbe		INPUT_ERROR
 	jmp		MATH
 
-INPUT_ERROR:										; Input numbers error message
 
-; Error Message
+INPUT_ERROR:										; JMP From: line-87 or 90
+
+; Error Message										; Input numbers error message
 	mov		edx, OFFSET	errPrompt
 	call	WriteString
 	call	CrLf
 	Jmp		START_LOOP
 
-MATH:												; Input tests pass  ---  Start Math section
 
-; Add and print for A and B							; Calc and print A and B
+MATH:												; JMP From: line-91 - Input tests pass  ---  Start Math section
+
+; Add and print for A and B							; Calc and print A + B
 	mov		eax, numA
-	call	WriteDec
+	call	WriteInt
 	mov		edx, OFFSET addSym
 	call	WriteString
 	mov		eax, numB
-	call	WriteDec
+	call	WriteInt
 	mov		edx, OFFSET eqlSym
 	call	WriteString
 	add		eax, numA
-	call	WriteDec
+	call	WriteInt
 	call	CrLf
 
-; Subtract and print for A and B
+; Subtract and print for A and B					; Calc and print A - B
 	mov		eax, numA
-	call	WriteDec
+	call	WriteInt
 	mov		edx, OFFSET subSym
 	call	WriteString
 	mov		eax, numB
-	call	WriteDec
+	call	WriteInt
 	mov		edx, OFFSET eqlSym
 	call	WriteString
 	mov		eax, numA
 	sub		eax, numB
-	call	WriteDec
+	call	WriteInt
 	call	CrLf
 
-; Add and print for A and C							; Calc and print A and C
+; Add and print for A and C							; Calc and print A + C
 	mov		eax, numA
-	call	WriteDec
+	call	WriteInt
 	mov		edx, OFFSET addSym
 	call	WriteString
 	mov		eax, numC
-	call	WriteDec
+	call	WriteInt
 	mov		edx, OFFSET eqlSym
 	call	WriteString
 	add		eax, numA
-	call	WriteDec
+	call	WriteInt
 	call	CrLf
 
-; Subtract and print for A and C
+; Subtract and print for A and C					; Calc and print A - C
 	mov		eax, numA
-	call	WriteDec
+	call	WriteInt
 	mov		edx, OFFSET subSym
 	call	WriteString
 	mov		eax, numC
-	call	WriteDec
+	call	WriteInt
 	mov		edx, OFFSET eqlSym
 	call	WriteString
 	mov		eax, numA
 	sub		eax, numC
-	call	WriteDec
+	call	WriteInt
 	call	CrLf
 
-; Add and print for B and C							; Calc and print B and C
+; Add and print for B and C							; Calc and print B + C
 	mov		eax, numB
-	call	WriteDec
+	call	WriteInt
 	mov		edx, OFFSET addSym
 	call	WriteString
 	mov		eax, numC
-	call	WriteDec
+	call	WriteInt
 	mov		edx, OFFSET eqlSym
 	call	WriteString
 	add		eax, numB
-	call	WriteDec
+	call	WriteInt
 	call	CrLf
 
-; Subtract and print for B and C
+; Subtract and print for B and C					; Calc and print B - C
 	mov		eax, numB
-	call	WriteDec
+	call	WriteInt
 	mov		edx, OFFSET subSym
 	call	WriteString
 	mov		eax, numC
-	call	WriteDec
+	call	WriteInt
 	mov		edx, OFFSET eqlSym
 	call	WriteString
 	mov		eax, numB
 	sub		eax, numC
-	call	WriteDec
+	call	WriteInt
 	call	CrLf
 
-; Add and print for A and B and C					; Calc and print A B C
+; Add and print for A and B and C					; Calc and print A + B + C
 	mov		eax, numA
-	call	WriteDec
+	call	WriteInt
 	mov		edx, OFFSET addSym
 	call	WriteString
 	mov		eax, numB
-	call	WriteDec
+	call	WriteInt
 	mov		edx, OFFSET addSym
 	call	WriteString
 	mov		eax, numC
-	call	WriteDec
+	call	WriteInt
 	mov		edx, OFFSET eqlSym
 	call	WriteString
 	add		eax, numA
 	add		eax, numB
-	call	WriteDec
+	call	WriteInt
+	call	CrLf
+
+; Subtract and print for B and A					; Calc and print B - A
+	mov		eax, numB
+	call	WriteInt
+	mov		edx, OFFSET subSym
+	call	WriteString
+	mov		eax, numA
+	call	WriteInt
+	mov		edx, OFFSET eqlSym
+	call	WriteString
+	mov		eax, numB
+	sub		eax, numA
+	call	WriteInt
+	call	CrLf
+
+; Subtract and print for C and A					; Calc and print C - A
+	mov		eax, numC
+	call	WriteInt
+	mov		edx, OFFSET subSym
+	call	WriteString
+	mov		eax, numA
+	call	WriteInt
+	mov		edx, OFFSET eqlSym
+	call	WriteString
+	mov		eax, numC
+	sub		eax, numA
+	call	WriteInt
+	call	CrLf
+
+; Subtract and print for C and B					; Calc and print C - B
+	mov		eax, numC
+	call	WriteInt
+	mov		edx, OFFSET subSym
+	call	WriteString
+	mov		eax, numB
+	call	WriteInt
+	mov		edx, OFFSET eqlSym
+	call	WriteString
+	mov		eax, numC
+	sub		eax, numB
+	call	WriteInt
+	call	CrLf
+
+; Subtract and print for C and B and A				; Calc and print C - B - A
+	mov		eax, numC
+	call	WriteInt
+	mov		edx, OFFSET subSym
+	call	WriteString
+	mov		eax, numB
+	call	WriteInt
+	mov		edx, OFFSET subSym
+	call	WriteString
+	mov		eax, numA
+	call	WriteInt
+	mov		edx, OFFSET eqlSym
+	call	WriteString
+	mov		eax, numC
+	sub		eax, numB
+	sub		eax, numA
+	call	WriteInt
 	call	CrLf
 
 ; Prompt for quit									; Ask the user to quit
@@ -206,9 +278,9 @@ MATH:												; Input tests pass  ---  Start Math section
 	jmp		START_LOOP
 	
 
-CONTINUE:											; Print the final message when 1 entered
+CONTINUE:											; JMP From: line-270
 
-; Say "Good-bye"
+; Say "Good-bye"									; Print the final message when 1 entered
 	call	CrLf
 	mov		edx, OFFSET goodBye
 	call	WriteString
