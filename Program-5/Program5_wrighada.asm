856df6394ba501e908ea56d79ec2e45cff0db752
Adam Wright <adam@adamjwright.com>
2020-02-18 16:05:20 -0800

    print messages clean up data segment

--- Program-5/Program5_wrighada.asm
+++ Program-5/Program5_wrighada.asm
@@ -28,29 +28,28 @@ PARAM_1 EQU [ebp + 8]																; Explicit stack offset for parameter 1
 PARAM_2 EQU [ebp + 12]																; Explicit stack offset for parameter 2
 PARAM_3 EQU [ebp + 16]																; Explicit stack offset for parameter 3
 PARAM_4 EQU [ebp + 20]																; Explicit stack offset for parameter 4
-PARAM_5 EQU [ebp + 24]																; Explicit stack offset for parameter 5
-PARAM_6 EQU [ebp + 28]																; Explicit stack offset for parameter 6
-PARAM_7 EQU [ebp + 32]																; Explicit stack offset for parameter 7
 
 
 .data																				; VARIABLE DEFINITIONS
-intro		BYTE	"** Program-5 -- Array Sorting **", 0
-programmer	BYTE	"** Programmed by Adam Wright  **", 0
-instr1		BYTE	"This program generates 200 random numbers in the range ", 0 
-instr2		BYTE	"[10 ... 29], displays the original list, ", 0
-instr3		BYTE	"sorts the list, displays the median value, displays ", 0 
-instr4		BYTE	"the list sorted in ascending order, then ", 0
-instr5		BYTE	"displays the number of instances of each generated value.", 0
-numPrompt	BYTE	"Enter the number of composites to display [1 .. 400]: ", 0
-errPrompt	BYTE	"Number Invalid!", 0
+intro		BYTE	"** Program-5 -- Array Sorting **", 0dh, 0ah, 0
+programmer	BYTE	"** Programmed by Adam Wright  **", 0dh, 0ah, 0
+instruct	BYTE	"This program generates 200 random numbers in the range "
+			BYTE	"[10 ... 29], displays the original list, ", 0dh, 0ah
+			BYTE	"sorts the list, displays the median value, displays "
+			BYTE	"the list sorted in ascending order, then ", 0dh, 0ah
+			BYTE	"displays the number of instances of each generated "
+			BYTE	"value.", 0dh, 0ah, 0
+unsortMsg	BYTE	"Your unsorted random numbers:", 0
+medianMsg	BYTE	"List Median:", 0
+sortMsg		BYTE	"Your sorted random numbers:", 0
+listMsg		BYTE	"Your list of instances of each generated number, "
+			BYTE	"starting with the number of 10s:", 0
 quitPrompt	BYTE	"Press 1 to quit and 2 to continue: ", 0
 byePrompt	BYTE	"Good-bye, and thanks for using my program!", 0
 array		DWORD	200 DUP(?)														; Empty array of DWORDS to hold the number array
-numCounts	DWORD	20 DUP(?)														; Empty array 
-quitVal		DWORD	1																; Integer holding 1 to quit or any other value to continue
-curRand		DWORD	?																; Integer holding the current random number
-curVal		DWORD	4																; Integer holding the composite to be checked and printed
+numCounts	DWORD	20 DUP(?)														; Empty array
 median		DWORD	0																; Integer to receive the calculated median
+quitVal		DWORD	1																; Integer holding 1 to quit or any other value to continue
 
 																					
 .code																				; EXECUTABLE INSTRUCTIONS
@@ -60,11 +59,7 @@ main PROC
 	call	Randomize
 
 ; Introduce title, programmer, and instructions
-	push	OFFSET instr5
-	push	OFFSET instr4
-	push	OFFSET instr3
-	push	OFFSET instr2
-	push	OFFSET instr1
+	push	OFFSET instruct
 	push	OFFSET programmer
 	push	OFFSET intro
 	call	introduction
@@ -79,6 +74,9 @@ MAIN_LOOP:																			; Restart (quitVal == 1) JMP From: line-
 	call	fillArray
 
 ; Print the unsorted array
+	push	OFFSET unsortMsg
+	push	ARRAY_SIZE
+	push	OFFSET array
 	call	displayList
 
 ; Sort the array from low to high
@@ -88,12 +86,18 @@ MAIN_LOOP:																			; Restart (quitVal == 1) JMP From: line-
 	call	displayMedian
 
 ; Print the sorted array
+	push	OFFSET sortMsg
+	push	ARRAY_SIZE
+	push	OFFSET array
 	call	displayList
 
 ; Calculate number occurances 10-19
 	call	countList
 
 ; Print the number counts 10-29
+	push	OFFSET listMsg
+	push	ARRAY_SIZE
+	push	OFFSET array
 	call	displayList
 
 ; Ask if the user wants to quit
@@ -125,42 +129,29 @@ main ENDP
 ; Pre-conditions:	 4 string pointers pushed onto stack
 ; Post-conditions:	 none
 ; Parameters:		 PARAM_1: OFFSET intro, PARAM_2: OFFSET programmer
-;					 PARAM_3: OFFSET instr1, PARAM_4: OFFSET instr2
-;					 PARAM_5: OFFSET instr3, PARAM_6: OFFSET instr4
-;					 PARAM_7: OFFSET instr5
-; Registers changed: edx
+;					 PARAM_3: OFFSET instruct
+; Registers changed: edx, ebp, esp
 ;------------------------------------------------------------------------
 
 introduction PROC
 
-; Introduce title, programmer
-	call	CrLf
+; Print the title message
 	push	ebp
 	mov		ebp, esp
+	call	CrLf
 	mov		edx, PARAM_1
 	call	WriteString
-	call	CrLf
+
+; Print the programmer message
 	mov		edx, PARAM_2
 	call	WriteString
 	call	CrLf
-	call	CrLf
 
-; Print the instructions with formatting for MSVS window width
+; Print the instructions and finish
 	mov		edx, PARAM_3
 	call	WriteString
-	mov		edx, PARAM_4
-	call	WriteString
-	call	CrLf
-	mov		edx, PARAM_5
-	call	WriteString
-	mov		edx, PARAM_6
-	call	WriteString
-	call	CrLf
-	mov		edx, PARAM_7
-	call	WriteString
-	call	CrLf
 	pop		ebp
-	ret		28
+	ret		3 * TYPE PARAM_1
 
 introduction ENDP
 
@@ -173,37 +164,84 @@ introduction ENDP
 ; Post-conditions:	 array filled with 200 values in range 10-29
 ; Parameters:		 PARAM_1: OFFSET array, PARAM_2: RANGE_LO (value)
 ;					 PARAM_3: RANGE_HI (value) PARAM_4: ARRAY_SIZE (value)
-; Registers changed: edx, eax
+; Registers changed: eax, esi, ebp, esp
 ;------------------------------------------------------------------------------
 
 fillArray PROC
 
-; Set up
+; Set up array filling loop
 	push	ebp
 	mov		ebp, esp
+	mov		edi, PARAM_1
+	mov		ecx, PARAM_4
 
+ARRAY_FILL:
 
+; Generate number between 10-29
+	mov		eax, PARAM_3
+	sub		eax, PARAM_2
+	inc		eax
+	call	RandomRange
+	add		eax, PARAM_2
+
+; Enter number into array
+	mov		[edi], eax
+	add		edi, TYPE array
+	loop	ARRAY_FILL
+
+; Exit when array length is reached
 	pop		ebp
-	ret		16
+	ret		4 * TYPE PARAM_1
 
 fillArray ENDP
 
 
-;------------------------------------------------------------------------------
+;--------------------------------------------------------------------------------
 ; sortList
 ;
-; Description:        Sorts array from low to high
-; Pre-conditions:	  array and ARRAY_SIZE on stack
+; Description:        Sorts array of 32 bit ints from low to high with bubblesort
+;					  from pg.375 of ed.7 Intel Assembly by Irvine
+; Pre-conditions:	  OFFSET array and ARRAY_SIZE on stack
 ; Post-conditions:	  array sorted from low to high
-; Parameters:		  
-; Registers changed:  eax
-;------------------------------------------------------------------------------
+; Parameters:		  PARAM_1: OFFSET array, PARAM_2: ARRAY_SIZE
+; Registers changed:  eax, ecx, ebp, esi
+;--------------------------------------------------------------------------------
 
 sortList PROC
 
-	call	exchangeElements
+; Set array loop to array address
+;	push	ebp
+;	mov		ebp, esp
+;	mov		ecx, PARAM_2
+;	dec		ecx						; decrement count by 1
 
-	ret
+L1:
+
+;	push	ecx						; save outer loop count
+;	mov		esi, PARAM_1			; point to first value
+
+L2:
+
+;	mov		eax, [esi]				; get array value
+;	cmp		[esi+4], eax			; compare a pair of values
+;	jg		L3						; if [ESI] <= [ESI+4], no exchange
+
+; Call swap function
+;	push	
+;	xchg	eax, [esi+4]			; exchange the pair
+;	mov		[esi], eax
+
+L3:
+
+;	add		esi, 4					; move both pointers forward
+;	loop	L2						; inner loop
+;	pop		ecx						; retrieve outer loop count
+;	loop	L1						; else repeat outer loop
+
+L4:
+
+;	pop		ebp
+;	ret		2 * TYPE PARAM_1
 
 sortList ENDP
 
@@ -214,7 +252,7 @@ sortList ENDP
 ; Description:        Swaps two values by reference
 ; Pre-conditions:	  Two array indexes on stack
 ; Post-conditions:	  Paramater 1 and 2 are swapped
-; Parameters:		  
+; Parameters:		  PARAM_1: , PARM_2: 
 ; Registers changed:  eax, ebx
 ;------------------------------------------------------------------------------
 
@@ -233,7 +271,7 @@ exchangeElements ENDP
 ; Description:        Calculates and prints the median value of the array
 ; Pre-conditions:	  curVal contains 4, compCheck contains 0
 ; Post-conditions:	  Median printed to console
-; Parameters:		  
+; Parameters:		  PARAM_1: , PARM_2: 
 ; Registers changed:  eax, ebx, edx
 ;------------------------------------------------------------------------------
 
@@ -246,21 +284,32 @@ displayMedian PROC
 displayMedian ENDP
 
 
-;------------------------------------------------------------------------------
+;--------------------------------------------------------------------------------
 ; displayList
 ;
 ; Description:        Print the array 20 numbers per line and two space columns
-; Pre-conditions:	  array and ARRAY_SIZE on stack
+; Pre-conditions:	  OFFSET array, ARRAY_SIZE, unsortMsg on stack
 ; Post-conditions:	  array printed to console
-; Parameters:		  PARAM_1: OFFSET array, PARAM_2: ARRAY_SIZE
+; Parameters:		  PARAM_1: OFFSET array, PARAM_2: ARRAY_SIZE 
+;					  PARAM_3: printing message (unsortMsg, sortMsg, or listMsg)
 ; Registers changed:  eax, edx
-;------------------------------------------------------------------------------
+;--------------------------------------------------------------------------------
 
 displayList PROC
 
+; Set up
+	call	CrLf
+	push	ebp
+	mov		ebp, esp
 
+; Print the unsortMsg
+	mov		edx, PARAM_3
+	call	WriteString
+	call	CrLf
 
-	ret
+; Exit after array printed
+	pop		ebp
+	ret		3 * TYPE PARAM_1
 
 displayList ENDP
 
@@ -292,7 +341,7 @@ countList ENDP
 ; Pre-conditions:	  quitPrompt pushed onto stack
 ; Post-conditions:	  quitVal stored in eax upon return
 ; Parameters:		  PARAM_1: OFFSET quitPrompt
-; Registers changed:  edx, eax
+; Registers changed:  edx, eax, epb, esp
 ;------------------------------------------------------------------------------
 
 quit PROC
@@ -307,7 +356,7 @@ quit PROC
 	call	WriteString
 	call	ReadInt
 	pop		ebp
-	ret		4
+	ret		1 * TYPE PARAM_1
 
 quit ENDP
 
@@ -319,7 +368,7 @@ quit ENDP
 ; Pre-conditions:	  byePrompt pushed onto stack
 ; Post-conditions:	  none
 ; Parameters:		  PARAM_1: OFFSET byePrompt
-; Registers changed:  edx
+; Registers changed:  edx, ebp, esp
 ;------------------------------------------------------------------------------
 
 farewell PROC															
@@ -334,7 +383,7 @@ farewell PROC
 	call	WriteString
 	call	CrLf
 	pop		ebp
-	ret		4
+	ret		1 * TYPE PARAM_1
 
 farewell ENDP
 
